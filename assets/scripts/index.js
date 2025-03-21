7d7bbd4845e4b7fce6d1f62b0cd9735c430fcf99
Durga Prasad <durgaprasad97005@gmail.com>
2025-03-21 11:50:50 +0530

    Updated commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,16 +6,20 @@ rotateClockHands = () => {
     const date = new Date();
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    const seconds = date.getSeconds();    
+    const seconds = date.getSeconds();
+    const milliSeconds = date.getMilliseconds();
 
     // Calculating degrees of rotation for clock hands
-    hrotation = (30 * hours) + (minutes / 2) + (seconds / 120);
-    mrotation = (6 * minutes) + (seconds / 10);
-    srotation = (6 * seconds);
+    hrotation = (30 * hours) + (minutes / 2) + (seconds / 120) + (milliSeconds / 120000);
+    mrotation = (6 * minutes) + (seconds / 10) + (milliSeconds / 10000);
+    srotation = (6 * seconds) + (6 * milliSeconds / 1000);
 
     hourHand.style.transform = `rotate(${hrotation}deg)`;
     minuteHand.style.transform = `rotate(${mrotation}deg)`;
     secondHand.style.transform = `rotate(${srotation}deg)`;
+
+    console.log(milliSeconds);
+    
 }
 
-setInterval(rotateClockHands, 1000);
+setInterval(rotateClockHands, 1);
